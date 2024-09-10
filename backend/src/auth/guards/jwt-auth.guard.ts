e59ef4bf5e4f0f6cb1bcdb4428aeb7d72d6b64ba
Ehtasham Nasir <ehtasham.nasir89@gmail.com>
2024-09-10 10:19:56 +0400

    dockerized frontend and backend

--- backend/src/auth/guards/jwt-auth.guard.ts
+++ backend/src/auth/guards/jwt-auth.guard.ts
@@ -1,17 +1,9 @@
-import {
-  Injectable,
-  ExecutionContext,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  handleRequest(err, user, info, context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-
-    console.log('Cookies Test: ', request.cookies);
-
+  handleRequest(err, user, info) {
     if (err || !user) {
       console.error('JWT Auth Error:', err || info);
       throw err || new UnauthorizedException();
